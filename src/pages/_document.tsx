063ad312dcec2c5847fd70dde5e1356d30ce7a76
ayadag <109915071+ayadag@users.noreply.github.com>
2024-03-29 16:42:31 +0300

    web1

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Document, { DocumentContext, Head, Html, Main, NextScript } from "next/document";
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext){
+    const initialProps = await Document.getInitialProps(ctx);
 
-const _document = () => {
-  return (
-    <div>_document</div>
-  )
+    return initialProps;
+  }
+
+  render(){
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" href="/favicon.icon"/>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default _document
+export default MyDocument;
